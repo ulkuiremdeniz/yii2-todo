515dcff6ad1026ed7c585a3fc4a1db1f9e7bf501
ulkuiremdeniz <ulkuiremdeniz@gmail.com>
2023-07-31 18:23:16 +0300

    zil ikonu.

--- src/widgets/views/task.php
+++ src/widgets/views/task.php
@@ -4,58 +4,48 @@ use ulkuiremdeniz\todo\bundles\TaskAsset;
 
 TaskAsset::register($this);
 
+// Bildirim durumuna göre zil ikonunun rengini belirlemek için bir değişken oluşturuyoruz.
+$isTask = !empty($tasks);
 ?>
 
+<style>
+    .fa-bell {
+        font-size: 1.5rem;
+        /* Zil ikonunun rengini dinamik olarak değiştirmek için bir değişken tanımlıyoruz */
+        --bell-icon-color: <?= $isTask ? 'red' : 'white' ?>;
+        color: var(--bell-icon-color);
+    }
+</style>
+
 <div class="dropdown">
-    <!-- data-bs-toggle ve data-bs-target, bildirimlerin açılıp-kapanması için kullanılır. href tıklandığında yönlendirilecek URL'yi belirtir -->
-
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Sayfa Başlığı</title>
-        <!-- Bootstrap Icons CSS dosyasını ekleyin -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.18.0/font/bootstrap-icons.css" rel="stylesheet">
-    </head>
-    <body>
-    <a id="dLabel" role="button" data-bs-toggle="dropdown" data-bs-target="#" href="#">
-        <!-- Bildirim simgesini temsil eden ikon ekler (zil sembolü) -->
-        <i class="bi-bell"></i>
+    <a href="#" class="dropdown-toggle btn " data-bs-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+        <i class="fa fa-bell"></i>
     </a>
-    </body>
-    </html>
-
-
     <!-- Bildirimlerin gösterileceği menü listesi tanımlandı. "dropdown-menu" class'ı menünün bir açılır menü olduğunu belirtmek için eklenmiştir -->
     <ul class="dropdown-menu task" aria-labelledby="dLabel">
-        <!-- Menü başlığı "task" olarak belirtilmiştir. "dropdown-item" class'ı Bootstrap 5 için geçerlidir -->
-        <li><a class="dropdown-item task-heading" href="#">Task<span class="float-end">View all<i class="bi bi-arrow-right"></i></span></a></li>
+        <!-- Menü başlığı "task" olarak belirtilmiştir.-->
+        <li><a class="dropdown-item task-heading task-footer" href="#">Task<span class="float-end">View all<i class="bi bi-arrow-right"></i></span></a></li>
         <!-- Menüdeki ögeleri ayırmak için ayırıcı eklenmiştir -->
         <li><hr class="dropdown-divider"></li>
         <!-- Bildirimlerin listeleneceği bölüm -->
-
-            <!-- Her bir bildirimin ayrı bağlantı içinde -->
-
-            <?php if (!empty($tasks)): ?>
+        <?php if ($isTask): ?>
+            <!-- Eğer '$tasks' değişkeni doluysa, yani görevler varsa, görevleri listeliyoruz -->
             <?php foreach ($tasks as $task): ?>
-        <li>
-            <!-- Her bir bildirimin ayrı bağlantı içinde -->
-            <a class="dropdown-item content" href="#">
-                <!-- Her bir bildirimin içeriğini oluşturan bir bölümdür -->
-                <div class="task-item">
-                    <h4 class="item-title"><?= $task->title ?></h4>
-                    <p class="item-info"><?= $task->description ?></p>
-                </div>
-            </a>
-        </li>
-        <li><hr class="dropdown-divider"></li>
-        <?php endforeach; ?>
-        <?php else: ?>
-
+                <li>
+                    <!-- Her bir bildirimin ayrı bağlantı içinde -->
+                    <a class="dropdown-item content" href="#">
+                        <!-- Her bir bildirimin içeriğini oluşturan bir bölümdür -->
+                        <div class="task-item">
+                            <h4 class="item-title"><?= $task['title'] ?></h4>
+                            <p class="item-info"><?= $task['description'] ?></p>
+                        </div>
+                    </a>
+                       <hr class="dropdown-divider">     <!--  Sadece görevlerin ardından ayırıcı ekleniyor -->
+                </li>
+                <!-- Diğer bildirimler buraya eklenir -->
+        <!--   <li><hr class="dropdown-divider"></li> -->
+            <?php endforeach; ?>
         <?php endif; ?>
-
-
-        <!-- Diğer bildirimler buraya eklenir -->
-        <li><hr class="dropdown-divider"></li>
         <!-- "View all" tüm bildirimleri görüntülemek için kullanılır -->
         <li><a class="dropdown-item task-footer" href="#">View all<span class="float-end"><i class="bi bi-arrow-right"></i></span></a></li>
     </ul>
